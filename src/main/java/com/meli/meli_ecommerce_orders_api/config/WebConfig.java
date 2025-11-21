cdb863b81b93f24b889279ade12738e28c30225b
AlduinoCalderon <aldocalderon97@gmail.com>
2025-11-21 00:19:58 -0600

    feat: read CORS_ORIGIN from environment variable

--- src/main/java/com/meli/meli_ecommerce_orders_api/config/WebConfig.java
+++ src/main/java/com/meli/meli_ecommerce_orders_api/config/WebConfig.java
@@ -1,22 +1,33 @@
 package com.meli.meli_ecommerce_orders_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+    
+    @Value("${CORS_ORIGIN:http://localhost:5173}")
+    private String corsOrigin;
+    
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                var config = registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
+                if ("*".equals(corsOrigin)) {
+                    config.allowedOriginPatterns("*")
+                          .allowCredentials(false);
+                } else {
+                    config.allowedOrigins(corsOrigin)
+                          .allowCredentials(true);
+                }
             }
         };
     }
